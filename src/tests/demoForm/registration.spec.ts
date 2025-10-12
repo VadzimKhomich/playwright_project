18a2104a5c06cd411db6fa05467412afaf44c743
Vadzim Khomich <vadzim.khomich@activeplatform.com>
2025-10-12 16:03:36 +0300

    path fix test2

--- src/tests/demoForm/registration.spec.ts
+++ src/tests/demoForm/registration.spec.ts
@@ -116,21 +116,3 @@ test.describe("[anatoly-karpovich] [registration SMOKE]", () => {
     expect((await password.innerText()).length).toBe(user.password.length);
   });
 });
-
-test.describe("[anatoly-karpovich] [login]", () => {
-  const user = {
-    name: "test@gmail.com",
-    password: "SecretPw123!@#",
-  };
-  test("Check login after add cadentials to local storage", async ({ page }) => {
-    await page.goto("https://anatoly-karpovich.github.io/demo-login-form/");
-    await page.evaluate((user) => {
-      localStorage.setItem(user.name, JSON.stringify(user));
-    }, user);
-    await page.locator("#userName").fill(user.name);
-    await page.locator("#password").fill(user.password);
-    await page.locator("#submit").click();
-
-    await expect(page.locator("#successMessage"), "Check messege after login").toHaveText(`Hello, ${user.name}!`);
-  });
-});
